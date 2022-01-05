fda2fb7ffb6e79bfa1573fcab8ba516447371ecf
백은주 <specialguest@live.co.kr>
2022-01-05 21:48:33 +0900

    chore: Lunr 설정

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,5 @@
 import 'typeface-montserrat';
+
 import 'prismjs/themes/prism-coy.css';
 import 'prismjs/plugins/command-line/prism-command-line.css';
 import 'prismjs/plugins/line-highlight/prism-line-highlight.css';
