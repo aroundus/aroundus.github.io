fba752e1fa62a160d0fcd005ab94b476d38d36f1
백은주 <eunju.paik@wadiz.kr>
2025-03-03 11:35:38 +0900

    fix: 오픈그래프 정보를 반영하지 않는 문제

--- src/features/helmet/ui/Helmet.tsx
+++ src/features/helmet/ui/Helmet.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { graphql, useStaticQuery } from 'gatsby';
-import { Helmet as ReactHelmet } from 'react-helmet-async';
+import { Helmet as ReactHelmet } from 'react-helmet';
 
 interface Data {
   site: {
